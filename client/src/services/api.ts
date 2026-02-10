24547119aab00a8d7f3d43fe751e7f4bf4d03e56
Dongwon Henry Seon <123602432+boolYikes@users.noreply.github.com>
2026-02-10 18:30:38 +0000

    workflow test 13 #no-tag #no-alarm

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:8000/api",
+  // baseURL: import.meta.env.VITE_API_BASE || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE ?? "/api",
   headers: {
     "Content-Type": "application/json",
   },
